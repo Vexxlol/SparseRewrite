a19a9b78ba9aa9f9bac2b344aa9543b51964e097
Cooper <61489794+Laudre@users.noreply.github.com>
2020-05-24 20:58:51 +1000

    Just fixed copy paste

--- cogs/fun.py
+++ cogs/fun.py
@@ -9,7 +9,7 @@ class Fun(commands.Cog):
     @commands.command(name="8ball")
     async def _8ball(self, ctx):
         """
-        sends the bot latency
+        Rnadom 8ball Command, Replys with the following below to your question(s)
         """
         random_response = ["My soruces are telling me no", "No", "Try again later", "Negative", "Im sorry, but i dont understand", "Please repeat", "The answer shall remain unknown", "Yes", "Postitive", "My sources are telling me yes."]
         await ctx.send(r.choice(random_response))
