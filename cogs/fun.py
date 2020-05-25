06cb7f806267d2ffc854625fd266f796391e05b5
Cooper <61489794+Laudre@users.noreply.github.com>
2020-05-25 21:49:02 +1000

    test

--- cogs/fun.py
+++ cogs/fun.py
@@ -13,6 +13,15 @@ class Fun(commands.Cog):
         """
         random_response = ["My soruces are telling me no", "No", "Try again later", "Negative", "Im sorry, but i dont understand", "Please repeat", "The answer shall remain unknown", "Yes", "Postitive", "My sources are telling me yes."]
         await ctx.send(r.choice(random_response))
+    
+    @commands.command(name="calculator")
+    async def _calculator(self, ctx, arg1, arg2)
+    """
+    Very Very basic command just for fun + to test webhook
+    """
+       results = float(arg1) * float(arg2)
+        await ctx.send(f"your results were {results}")
+        
         
 def setup(bot):
     bot.add_cog(Fun(bot))
