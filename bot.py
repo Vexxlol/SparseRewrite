ecfd65069f205dbf7270ab6e3976db48b7ce9568
Cooper <61489794+Penika@users.noreply.github.com>
2020-06-09 12:29:49 +1000

    added comments for docs

--- bot.py
+++ bot.py
@@ -1,15 +1,14 @@
 import sys, os
-from misc.injectENV import bang
-from SparseClient import client as c
+from misc.injectENV import bang#This may error, ignore it, it isnt harmful      
+from SparseClient import client as c # Imports SparseClient.py
 
 def run():
     bang()
-    client = c.SparseClient(prefix="-", token=os.getenv("token"), commandDirs="cogs")
-    client.loadCog("misc")
-    client.loadCog("fun")
-    # TODO: WORK ON MODERATION COG
-    #client.loadCog("moderation")
-    client.connect()
+    client = c.SparseClient(prefix="!", token=os.getenv("token"), commandDirs="cogs")#this checks woth sparseclient.py if you want to update the token and lavalink servers do it in the .env file
+    client.loadCog("misc")# Loads the cogs      
+    client.loadCog("fun")# Loads the cogs
+    # TODO: Work on sql querys for premium commands. Premium commands include emoji picker etc.
+    client.connect()#connects to discord 
 
 run()
 
