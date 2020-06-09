b43434e9888fd6f6f013aefc629676834b6905cd
Cooper <61489794+Penika@users.noreply.github.com>
2020-06-09 12:31:22 +1000

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -6,14 +6,7 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command(name="8ball")
-    async def _8ball(self, ctx):
-        """
-        Rnadom 8ball Command, Replys with the following below to your question(s)
-        """
-        random_response = ["My soruces are telling me no", "No", "Try again later", "Negative", "Im sorry, but i dont understand", "Please repeat", "The answer shall remain unknown", "Yes", "Postitive", "My sources are telling me yes."]
-        await ctx.send(r.choice(random_response))
-    
+   
    
         
 def setup(bot):
